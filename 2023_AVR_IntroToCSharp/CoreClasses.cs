e5bb485cb67730b33ec4bf55cd0281bab69fb4fe
Niall McGuinness <niall.mcguinness@dkit.ie>
2023-09-20 13:56:49 +0100

    Add project files.

---
+++ 2023_AVR_IntroToCSharp/CoreClasses.cs
@@ -0,0 +1,18 @@
+﻿namespace AVR
+{
+    public class AI
+    {
+        public void TextToSpeech(string text)
+        {
+        }
+    }
+
+    namespace Core
+    {
+        public class Network
+        {
+            public bool Send(string data)
+            { return false; }
+        }
+    }
+}
---
+++ 2023_AVR_IntroToCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using AVR.Core;
+using System;
+
+namespace AVR
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            Network n = new Network();
+            //  AVR.Core.Network n = new AVR.Core.Network();
+        }
+    }
+}
