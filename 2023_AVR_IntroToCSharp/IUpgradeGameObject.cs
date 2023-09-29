fb3fa885ba4df458e1337e617a157a5be5d31813
Niall McGuinness <niall.mcguinness@dkit.ie>
2023-09-29 09:55:09 +0100

    :sparkles: Added interface demo for health

---
+++ 2023_AVR_IntroToCSharp/IUpgradeGameObject.cs
@@ -0,0 +1,19 @@
+﻿using AVR.Entities;
+
+namespace AVR
+{
+    public interface IUpgradeGameObject
+    {
+        void Upgrade(Player p);
+    }
+
+    public class UpgradePlayerHealth : IUpgradeGameObject
+    {
+        public void Upgrade(Player p)
+        {
+            //if low, then upgrade
+            if (p.Health < 100)
+                p.Health += 100;
+        }
+    }
+}
